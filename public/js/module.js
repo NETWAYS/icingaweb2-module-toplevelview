bc72cf95c0ae8adceec4c30d5ead3a29cede3440
Markus Opolka <markus.opolka@netways.de>
2026-08-11 10:31:54 +0200

    Modernize JavaScript

--- public/js/module.js
+++ public/js/module.js
@@ -1,26 +1,23 @@
-;(function (Icinga) {
+;(function (Icinga, $) {
 
     'use strict';
 
-    var Toplevelview = function (module) {
-        this.module = module;
-        this.initialize();
-    };
+    class Toplevelview {
+        constructor(module) {
+            this.icinga = module.icinga;
 
-    Toplevelview.prototype = {
-        initialize: function () {
-            this.module.on('click', '.tlv-view-tree .tlv-tree-node', this.processTreeNodeClick);
-            this.module.on('click', 'div[href].action', this.buttonClick, this);
-            this.module.on('click', '.btn-remove', this.onRemoveClick, this);
-            this.module.on('rendered', this.rendered);
-        },
+            module.on('click', '.tlv-view-tree .tlv-tree-node', this.processTreeNodeClick);
+            module.on('click', 'div[href].action', this.buttonClick);
+            module.on('click', '.btn-remove', this.onRemoveClick);
+            module.on('rendered', this.rendered);
+        }
 
         /**
          * rendered adds the CodeMirror editor to the designated div
          */
-        rendered: function (ev) {
-            this.collapseOnLoad(ev);
-            var $container = $(ev.currentTarget);
+        rendered(event) {
+            this.collapseOnLoad(event);
+            var $container = $(event.currentTarget);
             $container.find('.codemirror').each(function (i, el) {
                 var mode = el.getAttribute('data-codemirror-mode');
                 var editor = CodeMirror.fromTextArea(el, {
@@ -38,12 +35,12 @@
                     'Ctrl-F': 'findPersistent'
                 });
             });
-        },
+        }
 
         /**
          * processTreeNodeClick toggles the collapsed status of the tree nodes
          */
-        processTreeNodeClick: function (event) {
+        processTreeNodeClick(event) {
             event.stopPropagation();
             var $el = $(event.currentTarget);
             var $parent = $el.parents('.tlv-tree-node');
@@ -55,29 +52,29 @@
                 $el.addClass('tlv-collapsed');
                 $all.addClass('tlv-collapsed');
             }
-        },
+        }
 
         /**
          * collapseOnLoad sets all OK nodes to collapsed,
          * because these are not as interesting.
          */
-        collapseOnLoad: function (event) {
+        collapseOnLoad(event) {
             var $el = $(event.currentTarget);
             $el.find('.tlv-view-tree .tlv-tree-node.tlv-collapsible.ok').addClass('tlv-collapsed');
-        },
+        }
 
         /**
          * onRemoveClick shows a Browser confirmation windows.
          */
-        onRemoveClick: function (event) {
+        onRemoveClick(event) {
             event.stopPropagation();
             let target = event.currentTarget;
             const confirmMsg = target.getAttribute('data-confirmation');
 
             return confirm(confirmMsg);
-        },
+        }
 
-        buttonClick: function (event) {
+        buttonClick (event) {
             event.stopPropagation();
             var $el = $(event.currentTarget);
             var $links = $el.find('a[href]');
@@ -85,8 +82,8 @@
                 $links[0].click();
             }
         }
-    };
+    }
 
     Icinga.availableModules.toplevelview = Toplevelview;
 
-}(Icinga));
+})(Icinga, jQuery);
